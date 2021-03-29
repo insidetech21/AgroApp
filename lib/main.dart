f71aaf682ab0c16522b60d6998494abf441a52ca
vasantbasutkar <49515236+vasantbasutkar@users.noreply.github.com>
2021-03-29 21:04:44 +0530

    Updates Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 void main(){
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false, // for removing the default banner from app
-      home: HomePage(),
-    )
+      MaterialApp(
+        debugShowCheckedModeBanner: false, // for removing the default banner from app
+        home: HomePage(),
+      )
   );
 }
 
@@ -17,12 +18,33 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Widget image_carousel = new Container(
+      height: 200.0,
+      child: new Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/c1.jpg'),
+          AssetImage('images/m1.jpeg'),
+          AssetImage('images/m2.jpg'),
+          AssetImage('images/w1.jpeg'),
+          AssetImage('images/w3.jpeg'),
+          AssetImage('images/w3.jpeg'),
+        ],
+        autoplay: true,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 1000),
+        dotSize: 4.0,
+        dotColor: Colors.red,
+        indicatorBgPadding: 8.0,
+      ),
+    );
     return Scaffold(
       appBar: new AppBar(
+        elevation: 0.0,
         backgroundColor: Colors.red,
         title: Text("Agro App"),
         actions: <Widget>[
-         new IconButton(icon: Icon(Icons.search,color: Colors.white,), onPressed: (){}),
+          new IconButton(icon: Icon(Icons.search,color: Colors.white,), onPressed: (){}),
           new IconButton(icon: Icon(Icons.shopping_cart,color: Colors.white,), onPressed: (){})
         ],
       ),
@@ -31,20 +53,18 @@ class _HomePageState extends State<HomePage> {
           children: <Widget>[
             // Header
             new UserAccountsDrawerHeader
-              (accountName: Text("Stimit Shah"),
-                accountEmail: Text("stimitshah111@gmail.com"),
-        
+              (accountName: Text("Vasant Basutkar"),
+              accountEmail: Text("vasantbasutkar143@gmail.com"),
+              currentAccountPicture: GestureDetector(
+                child: new CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(Icons.person,color: Colors.white,),
+                ),
 
-            currentAccountPicture: GestureDetector(
-              child: new CircleAvatar(
-                backgroundColor: Colors.grey,
-                child: Icon(Icons.person,color: Colors.white,),
               ),
-
-            ),
-          decoration: new BoxDecoration(
-            color: Colors.red
-             ),
+              decoration: new BoxDecoration(
+                  color: Colors.red
+              ),
             ),
 
             // Body
@@ -52,16 +72,16 @@ class _HomePageState extends State<HomePage> {
             InkWell(
               onTap: (){},
               child: ListTile(
-              title: Text('Home Page'),
-              leading: Icon(Icons.home),
-          ),
+                title: Text('Home Page'),
+                leading: Icon(Icons.home,color: Colors.deepPurple,),
+              ),
             ),
 
             InkWell(
               onTap: (){},
               child: ListTile(
                 title: Text('My Account'),
-                leading: Icon(Icons.person),
+                leading: Icon(Icons.person,color: Colors.deepPurple,),
               ),
             ),
 
@@ -69,7 +89,7 @@ class _HomePageState extends State<HomePage> {
               onTap: (){},
               child: ListTile(
                 title: Text('My Orders'),
-                leading: Icon(Icons.shopping_basket),
+                leading: Icon(Icons.shopping_basket,color: Colors.deepPurple,),
               ),
             ),
 
@@ -77,7 +97,7 @@ class _HomePageState extends State<HomePage> {
               onTap: (){},
               child: ListTile(
                 title: Text('Categoris'),
-                leading: Icon(Icons.dashboard),
+                leading: Icon(Icons.dashboard,color: Colors.deepPurple,),
               ),
             ),
 
@@ -95,7 +115,7 @@ class _HomePageState extends State<HomePage> {
               onTap: (){},
               child: ListTile(
                 title: Text('Settings'),
-                leading: Icon(Icons.settings),
+                leading: Icon(Icons.settings,color: Colors.blue,),
               ),
             ),
 
@@ -103,13 +123,19 @@ class _HomePageState extends State<HomePage> {
               onTap: (){},
               child: ListTile(
                 title: Text('About'),
-                leading: Icon(Icons.help),
+                leading: Icon(Icons.help,color: Colors.green,),
               ),
             ),
 
 
           ],
         ),
+      ),
+
+      body: new ListView(
+        children: <Widget>[
+          image_carousel
+        ],
       ),
     );
   }
